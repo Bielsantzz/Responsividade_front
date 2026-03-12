30667ec900228d97c492de5db3346405ad882556
GABRIEL HENRIQUE DE OLIVEIRA SANTOS <gabriel.santos935@senaisp.edu.br>
2026-03-12 11:23:22 -0300

    catalogo finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-//mport "./footer.css"
+import "./footer.css"
 
 export default function Footer() {
   return (
     <footer className="footer" id="contato">
       <div className="container footer__inner">
-        <p>©2026 - biel Santos — Mini projeto responsivo</p>
+        <p>©2026 - biel Santos — mini projeto responsivo</p>
         <a href="#sobre">Voltar ao topo</a>
       </div>
     </footer>
